50dda943b89c7314516522579a9b3ba5ce96443f
Vinícius Romualdo <viniromualdo082@gmail.com>
2022-12-15 13:44:17 -0300

    completing T6

--- T2/polinomiosSimultOrtg.m
+++ T2/polinomiosSimultOrtg.m
@@ -1,4 +1,4 @@
-function [c, p2] = polinomiosSimultOrtg(p0, p1, x)
+function [c, p21] = polinomiosSimultOrtg(p0, p1, x)
   alpha2 = 0;
   beta2 = 0;
 
@@ -13,6 +13,7 @@ function [c, p2] = polinomiosSimultOrtg(p0, p1, x)
   % p0xp1 = quadgk(p0xp1x, 0, 1);
   % p0p0 = quadgk(p0p0x, 0, 1);
 
+  % produto escalar definindo pela questão
   for i = 1:4
     p0xp1 = p0(x(i)) .* (x(i) .* p1(x(i)));
     p1xp1 = p1(x(i)) .* (x(i) .* p1(x(i)));
@@ -26,7 +27,8 @@ function [c, p2] = polinomiosSimultOrtg(p0, p1, x)
   p2 = @(x) (x - alpha2) .* p1(x) - beta2 .* p0(x);
   p21 = sym(p2);
 
-  p2(1)
-  p21(1)
+  x = 1;
+  printf("p_2(1) = %d\n", p2(x))
+  subs(p21)
   c = coeffs(p21);
 end
