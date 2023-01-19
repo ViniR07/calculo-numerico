714f2f6e5857982a03d5442e4440aae33eacb8a6
Vinícius Romualdo <viniromualdo082@gmail.com>
2023-01-19 12:00:24 -0300

    T3.5 resolution

--- zero-funcoes/metodoIterativo.m
+++ zero-funcoes/metodoIterativo.m
@@ -1,4 +1,4 @@
-function [x] = metodoIterativo(f, x0, lim)
+function [x] = metodoIterativo(f, x0, lim, showSteps)
     # x0 = chute inicial
     # f = função
     # x = vetor de soluções
@@ -7,10 +7,14 @@ function [x] = metodoIterativo(f, x0, lim)
     xi = x0;
     for i = 1:lim
         x(i) = f(xi);
-        fprintf('x_%d at %d iterations is :\n', i, i-1);
-        fprintf(' %.20f \n', x(i));
+        if showSteps
+            fprintf('x_%d at %d iterations is :\n', i, i-1);
+            fprintf(' %.24f \n', x(i));
+        end
         xi = x(i);
     end
-    % fprintf('x_5 is :\n');
-    % fprintf(' %11.8f \n', x(lim));
+    if !showSteps
+        fprintf('x_%d is :\n', lim);
+        fprintf(' %.20f \n', x(lim));
+    end
 end
